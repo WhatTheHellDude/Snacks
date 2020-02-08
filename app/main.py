c7e081a7dfda597e10a4cd3c9ce5024b9219a10a
Me <Me@MacBook-Pro.local>
2020-02-07 20:02:53 -0500

    change port to 80

--- app/main.py
+++ app/main.py
@@ -106,6 +106,6 @@ if __name__ == '__main__':
     bottle.run(
         application,
         host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '8080'),
+        port=os.getenv('PORT', '80'),
         debug=os.getenv('DEBUG', True)
     )
